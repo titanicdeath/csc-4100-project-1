0a710d7a336c1cbfc7996c846cac870c1c22afed
titanicdeath <aldensimmons3@gmail.com>
2026-04-02 21:30:03 -0500

    Initial commit for CSC 4100 Project 1

---
+++ wish.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// The one and only error message you are allowed to print
+void print_error() {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+int main(int argc, char *argv[]) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    // Interactive Mode Loop
+    if (argc == 1) {
+        while (1) {
+            printf("wish> ");
+            fflush(stdout); // Forces the prompt to print immediately
+
+            // getline() allocates memory for you and reads the whole line
+            read = getline(&line, &len, stdin);
+            
+            // If user hits Ctrl+D (EOF), exit gracefully
+            if (read == -1) {
+                break;
+            }
+
+            // Remove the newline character at the end of the input
+            if (line[read - 1] == '\n') {
+                line[read - 1] = '\0';
+            }
+
+            // Ignore empty inputs (user just hit enter)
+            if (strlen(line) == 0) {
+                continue;
+            }
+
+            // --- BUILT-IN COMMAND: exit ---
+            if (strcmp(line, "exit") == 0) {
+                exit(0);
+            }
+
+            // TODO: Phase 2 - Parse the input into arguments using strsep()
+            // TODO: Phase 2 - fork() a child process
+            // TODO: Phase 2 - Use execv() to run the command
+            // TODO: Phase 2 - wait() for the child to finish
+
+            // Echo what the user typed to prove it works:
+            printf("You typed: %s\n", line);
+        }
+    } 
+    // Batch Mode (If they pass a file like ./wish batch.txt)
+    else if (argc == 2) {
+        // TODO: Phase 4 - Open the file, read line by line instead of stdin
+        printf("Batch mode not yet implemented.\n");
+    } 
+    // Too many arguments
+    else {
+        print_error();
+        exit(1);
+    }
+
+    // Free the memory getline() allocated
+    free(line);
+    return 0;
+}
